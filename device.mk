61518e7c0be16b96fb9d745eec83f32603cf3084
Abhishek <153516999+JigenxOhtsusuki@users.noreply.github.com>
2024-09-11 18:00:23 +0000

    Revert "X2: Evolution X bring up"
    This reverts commit 3ba525503e436c0530635f62f43af72b5425c351.

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ PRODUCT_PRODUCT_VNDK_VERSION := current
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
